51b0775f702703086cd9ef3d81c6b1dcaae00f1a
Jed Nugal <nugaljed@gmail.com>
2022-05-06 16:04:12 -0700

    Testing todo.js
    Simple test to make sure that the Todo component is in the document.

---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function todo ({ }) {
+    return (
+        <div className="hi" data-testid='todo-1'>
+            Hello World!
+        </div>
+   )
+}
+
+export default todo;
